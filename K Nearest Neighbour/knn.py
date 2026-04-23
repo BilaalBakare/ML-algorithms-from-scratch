b96d3e921537f962207545c45bdd6f2a13a9c1cc
Bilaal <bilaalolushola@gmail.com>
2026-04-23 08:31:30 +0100

    repaired the implementation

--- K Nearest Neighbour/knn.py
+++ K Nearest Neighbour/knn.py
@@ -3,7 +3,7 @@ import pandas as pd
 from collections import Counter
 
 class Knn:
-    def __init__(self, k=3):
+    def __init__(self, k=5):
         self.k = k
     
     def fit(self, X_train, y_train):
@@ -14,35 +14,21 @@ class Knn:
         y_pred = []
 
         for i in X_test:
-            pred = self.predict_single(i)
+            pred = self._predict_single(i)
             y_pred.append(pred)
 
-        return y_pred
+        return np.array(y_pred)
     
-    def predict_single(self, X):
-        pair_list = []
+    def _predict_single(self, X):
+        distance = np.sqrt(np.sum((self.X_train - X) ** 2, axis=1))
+        distance_sorted_sliced = np.argsort(distance)[:self.k]
+        labels = self.y_train[distance_sorted_sliced]
+        print(np.array(labels))
 
-        for i in range(len(self.X_train)):
-            sample_pair = self.distance(X, self.X_train[i]), self.y_train[i]
-            pair_list.append(sample_pair)
-        
-        pair_list.sort(key=lambda x: x[0])
+        count = Counter(labels)
+        prediction = count.most_common(1)[0][0]
 
-        # fke = first K elements
-        fke = pair_list[:self.k]
-
-        count = Counter(category[1] for category in fke)
-
-        prediction = count.most_common(1)[0]
-
-        return prediction[1]
-    
-    def distance(self, X1, X2):
-        X1 = np.array(X1)
-        X2 = np.array(X2)
-
-        dist = np.sqrt(np.sum((X1-X2)**2))
-        return dist
+        return prediction
     
     def __repr__(self):
         return f"KNN(k={self.k})"
